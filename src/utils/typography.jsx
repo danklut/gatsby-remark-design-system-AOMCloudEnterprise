c2207a8f504626214a699099827b453af11ffd73
danklut <aomcloudenterprise@outlook.com>
2019-01-08 04:08:56 -0800

    Add files via upload

--- src/utils/typography.jsx
+++ src/utils/typography.jsx
@@ -1,33 +1,44 @@
-import Typography from 'typography';
+
+import Typography from 'typography'
 
 const typography = new Typography({
-  title: 'Design System',
-  baseFontSize: '16px',
-  baseLineHeight: 1.5,
-  headerFontFamily: ['Roboto', 'sans-serif'],
-  bodyFontFamily: ['Roboto', 'sans-serif'],
-  scaleRatio: 2.5,
-  headerWeight: 700,
-  googleFonts: [
-    {
-      name: 'Roboto',
-      styles: ['700', '400'],
-    },
+  title: 'LekoArts',
+  baseFontSize: '18px',
+  baseLineHeight: 1.45,
+  /* Use the system font stack as fallback */
+  headerFontFamily: [
+    'Montserrat',
+    '-apple-system',
+    'BlinkMacSystemFont',
+    'Segoe UI',
+    'Roboto',
+    'Helvetica',
+    'Arial',
+    'sans-serif',
+    'Apple Color Emoji',
+    'Segoe UI Emoji',
+    'Segoe UI Symbol',
   ],
+  bodyFontFamily: [
+    'Istok Web',
+    '-apple-system',
+    'BlinkMacSystemFont',
+    'Segoe UI',
+    'Roboto',
+    'Helvetica',
+    'Arial',
+    'sans-serif',
+    'Apple Color Emoji',
+    'Segoe UI Emoji',
+    'Segoe UI Symbol',
+  ],
+  scaleRatio: 2.441,
+  headerWeight: 700,
   overrideStyles: () => ({
     img: {
       marginBottom: 0,
     },
-    p: {
-      marginTop: '.5rem',
-      marginBottom: '1rem',
-    },
-    h2: {
-      marginTop: '3rem',
-      marginBottom: '1rem',
-      color: '#48ba6f',
-    },
   }),
-});
+})
 
-export default typography;
+export default typography
